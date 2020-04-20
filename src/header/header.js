bd597864ca36f1ff535d56e690bf24b27ad085f7
feres94 <feresfatnassi@gmail.com>
2020-04-20 12:49:26 +0100

    first commit

---
+++ src/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <div>logo</div>
+      <div>links</div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/main/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './main.css';
+
+const Main = () => {
+  return (
+    <main>
+      <div>container 1</div>
+      <div>container 2 </div>
+    </main>
+  );
+};
+
+export default Main;
